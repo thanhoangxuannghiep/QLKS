4b523c0994bd48932a2e9d45339a43d4c92e18f3
s4t0r1n <s4t0r1n@gmail.com>
2016-05-24 23:04:36 +0700

    Update
    Update ucRent, ucBill, ucService

---
+++ QuanlyKS_izzy/BUS/procPhuongPH.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DTO;
+using DevExpress.XtraEditors;
+
+namespace BUS
+{
+    public class procUCBill
+    {
+
+
+        public static int create(string[] _value)
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_value[i]))
+                    return i;
+            }
+
+            DTBill.create(_value);
+
+            return -1;
+        }
+
+        public static int update(string[] _set, string _where)
+        {
+            string _value = "";
+            //"NgayLap", "TongGia", "NhanVien", "Phong", "MaPhieuThue"
+            for (int i = 0; i < _set.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_set[i]))
+                    return i;
+
+
+            }
+            _value += @"NgayLap=N'" + _set[0] + "',";
+            _value += @"TongGia=N'" + _set[1] + "',";
+            _value += @"NhanVien=N'" + _set[2] + "',";
+            _value += @"Phong='" + _set[3] + "',";
+            _value += @"MaPhieuThue='" + _set[4] + "'";
+
+
+            DTBill.update(_value, _where);
+            return -1;
+        }
+
+        public static bool delbyid(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+                return false;
+
+            if (DTBill.deletebyId(int.Parse(id)))
+                return true;
+            return false;
+        }
+    }
+
+    public class procUCBillDetail
+    {
+
+
+        public static int create(string[] _value)
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_value[i]))
+                    return i;
+            }
+
+            DTBillDetail.create(_value);
+
+            return -1;
+        }
+
+        public static int update(string[] _set, string _where)
+        {
+            string _value = "";
+            //"NgayLap", "TongGia", "NhanVien", "Phong", "MaPhieuThue"
+            for (int i = 0; i < _set.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_set[i]))
+                    return i;
+
+
+            }
+            _value += @"MaHD=N'" + _set[0] + "',";
+            _value += @"MaDichVu=N'" + _set[1] + "',";
+            _value += @"NgaySuDung=N'" + _set[2] + "',";
+            _value += @"Phong='" + _set[3] + "'";
+
+
+
+            DTBillDetail.update(_value, _where);
+            return -1;
+        }
+
+        public static bool delbyid(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+                return false;
+
+            if (DTBillDetail.deletebyId(int.Parse(id)))
+                return true;
+            return false;
+        }
+    }
+
+    public class procUCRent
+    {
+        public static DataTable getAll()
+        {
+            return DTRent.getAll();
+        }
+
+        public static int create(string[] _value)
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_value[i]))
+                    return i;
+            }
+
+            DTRent.create(_value);
+            return -1;
+            //return 
+        }
+
+        public static int update(string[] _set, string _where)
+        {
+            string _value = "";
+
+            for (int i = 0; i < _set.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_set[i]))
+                    return i;
+
+
+            }
+            _value += @"TenPhieuThue=N'" + _set[0] + "',";
+            _value += @"TinhTrangPhieuThue=N'" + _set[1] + "',";
+            _value += @"MaPhong=N'" + _set[2] + "',";
+            _value += @"NgayLap='" + _set[3] + "',";
+            _value += @"NgayBatDau='" + _set[4] + "',";
+            _value += @"NgayKetThuc='" + _set[5] + "',";
+            _value += @"MaKH='" + _set[6] + "'";
+
+            DTRent.update("PHIEUTHUE", _value, _where);
+            return -1;
+        }
+
+        public static bool delbyid(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+                return false;
+
+            if (DTRent.deletebyId(int.Parse(id)))
+                return true;
+            return false;
+        }
+
+        public static DataTable getRoom()
+        {
+            string sql = "SELECT MaPhong, TenPhong, Tang FROM PHONG";
+            return DTRent.exec(sql);
+        }
+
+        public static DataTable getCustomer()
+        {
+            string sql = "SELECT * FROM KHACHHANG";
+            return DTRent.exec(sql);
+        }
+    }
+
+    public class procUCService
+    {
+        public static DataTable getAll()
+        {
+            return DTService.getAll();
+        }
+
+        public static int create(string[] _value)
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_value[i]))
+                    return i;
+            }
+
+            DTService.create(_value);
+            return -1;
+            //return 
+        }
+
+        public static int update(string[] _set, string _where)
+        {
+            string _value = "";
+
+            for (int i = 0; i < _set.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(_set[i]))
+                    return i;
+
+
+            }
+
+            _value += @"TenDichVu=N'" + _set[0] + "',";
+            _value += @"Gia=N'" + _set[1] + "'";
+
+
+            DTService.update("DICHVU", _value, _where);
+            return -1;
+        }
+
+        public static bool delbyid(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+                return false;
+
+            if (DTService.deletebyId(int.Parse(id)))
+                return true;
+            return false;
+        }
+    }
+
+    public class procLogin
+    {
+        public static int Login(string user, string pass)
+        {
+            if (String.IsNullOrWhiteSpace(user))
+                return -2;
+            if (String.IsNullOrWhiteSpace(pass))
+                return -3;
+
+            return DTLogin.getData(user, pass);
+        }
+    }
+}
---
+++ QuanlyKS_izzy/DTO/DTPhuongPH.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Security.Cryptography;
+using DAO;
+
+
+namespace DTO
+{
+    public class DTBill : DTObject
+    {
+        public static DataTable getHoaDon()
+        {
+            return DTObject.getAll("HOADON");
+        }
+
+        public static DataTable getChiTietHoaDon(int mhd)
+        {
+            string sql = "SELECT * FROM CHITIETHOADON WHERE MaHD=N'" + mhd.ToString() + "'";
+
+            return DTBill.exec(sql);
+        }
+
+        public static DataTable getNhanVien()
+        {
+            string sql = "SELECT * FROM NHANVIEN";
+
+            return DTBill.exec(sql);
+        }
+
+        public static DataTable getRoom()
+        {
+            return DTBill.exec("SELECT * FROM PHONG");
+        }
+
+        public static DataTable getRent()
+        {
+            return DTBill.exec("SELECT * FROM PHIEUTHUE");
+        }
+
+
+        public static bool create(string[] value)
+        {
+            string[] col = { "NgayLap", "TongGia", "NhanVien", "Phong", "MaPhieuThue" };
+            return DTObject.insert("HOADON", col, value);
+        }
+        public static bool update(string _set, string _where)
+        {
+
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                string sql = "UPDATE HOADON SET " + _set + " WHERE " + _where;
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool deletebyId(int id)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+                string sql = "DELETE FROM HOADON WHERE MaHoaDon = " + id.ToString();
+                string sql2 = "DELETE FROM CHITIETHOADON WHERE MaHD=N'" + id.ToString() + "'";
+                provider.ExecuteNonQuery(sql2);
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static DataTable exec(string query)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                return provider.ExecuteQuery_DataTble(query);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class DTBillDetail
+    {
+        public static bool create(string[] value)
+        {
+            string[] col = { "MaHD", "MaDichVu", "NgaySuDung", "Phong" };
+            return DTObject.insert("CHITIETHOADON", col, value);
+        }
+
+        public static bool update(string _set, string _where)
+        {
+
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                string sql = "UPDATE CHITIETHOADON SET " + _set + " WHERE " + _where;
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool deletebyId(int id)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+                //string sql = "DELETE FROM HOADON WHERE MaHoaDon = " + id.ToString();
+                string sql2 = "DELETE FROM CHITIETHOADON WHERE UUID=N'" + id.ToString() + "'";
+                //provider.ExecuteNonQuery(sql2);
+                return provider.ExecuteNonQuery(sql2);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+    public class DTService : DTObject
+    {
+        public static DataTable getAll()
+        {
+            return DTObject.getAll("DICHVU");
+        }
+
+        public static bool create(string[] value)
+        {
+            string[] col = { "TenDichVu", "Gia" };
+            return DTObject.insert("DICHVU", col, value);
+        }
+        public static bool update(string _set, string _where)
+        {
+
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                string sql = "UPDATE DICHVU SET " + _set + " WHERE " + _where;
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool deletebyId(int id)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+                string sql = "DELETE FROM DICHVU WHERE MaDichVu = " + id.ToString();
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+    public class DTRent : DTObject
+    {
+        public static DataTable getAll()
+        {
+            return DTObject.getAll("PHIEUTHUE");
+        }
+
+        public static bool create(string[] value)
+        {
+            string[] col = { "TenPhieuThue", "TinhTrangPhieuThue", "MaPhong", "NgayLap", "NgayBatDau", "NgayKetThuc", "MaKH" };
+            return DTObject.insert("PHIEUTHUE", col, value);
+        }
+        public static bool update(string _set, string _where)
+        {
+
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                string sql = "UPDATE PHIEUTHUE SET " + _set + " WHERE " + _where;
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static bool deletebyId(int id)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+                string sql = "DELETE FROM PHIEUTHUE WHERE MaPhieuThue = " + id.ToString();
+                return provider.ExecuteNonQuery(sql);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static DataTable exec(string query)
+        {
+            try
+            {
+                DataProvider provider = new DataProvider();
+
+                return provider.ExecuteQuery_DataTble(query);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class DTLogin : DTObject
+    {
+        private static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        public static int getData(string user, string pass)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                pass = GetMd5Hash(md5Hash, pass);
+            }
+
+            DataProvider provider = new DataProvider();
+            string sql = "SELECT * FROM TAIKHOAN WHERE Username=N'" + user + "' AND Password=N'" + pass + "'";
+
+            DataSet ds = provider.ExecuteQuery(sql);
+
+            if (ds.Tables[0].Rows.Count == 1)
+            {
+                return int.Parse(ds.Tables[0].Rows[0][0].ToString());
+            }
+
+            return -1;
+
+        }
+    }
+}
